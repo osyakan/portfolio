008d02838a4ab08a97b2a1ca45268fadca5822bf
お茶缶 <kusakabe.kan.v5@elms.hokudai.ac.jp>
2022-04-15 22:49:05 +0900

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:url_launcher/url_launcher.dart';
 
 
 void main() {
@@ -7,7 +10,11 @@ void main() {
 
 //後で下に移す
 class SecondRoute extends StatelessWidget{
-  const SecondRoute({Key? key}) : super(key: key);
+  // added
+  SecondRoute(this.child_list);
+  List<String> child_list;
+
+  // const SecondRoute({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
 
@@ -15,29 +22,33 @@ class SecondRoute extends StatelessWidget{
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
-    //コンテンツのタイトルと画像(Map型)
-    List mycontents = [
-      ['1', 'assets/programmer.png'],
-      ['2', 'assets/programmer.png'],
-      ['3', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-    ];
-
     return Scaffold(
       appBar: AppBar(
-        title: Text("secondpage"),
+        title: Text(child_list[0]),
         centerTitle: true,
       ),
-      body: Text("hogehoge"),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(screenWidth*0.08),
+          child: Column(
+            children: [
+              // Text("title"),
+              Image.asset(child_list[1], fit: BoxFit.cover),
+              Text(child_list[2]),
+              InkWell(
+                child: Text(child_list[3]),
+                onTap: () async{
+                  if (await canLaunch(child_list[3])){
+                    await launch(
+                      child_list[3],
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
@@ -76,19 +87,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
     //コンテンツのタイトルと画像(Map型)
     List mycontents = [
-      ['1', 'assets/programmer.png'],
-      ['2', 'assets/programmer.png'],
-      ['3', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
-      ['4', 'assets/programmer.png'],
+      ['title',
+        'assets/programmer.png',
+        '''
+        detail of the content
+        ''',
+        'https://github.com/osyakan'],
+      ['1', 'assets/programmer.png',
+        '''
+        どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
+        吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。
+        この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも
+        思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少
+        し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残ってい
+        る。第一毛をもって装飾されべきはずの顔がつるつるしてまるで薬缶だ。その後猫にもだいぶ逢ったがこんな片輪には一度も
+        出会わした事がない。のみならず顔の真中があまりに突起している。そうしてその穴の中から時々ぷうぷうと煙を吹く。どう
+        も咽せぽくて実に弱った。これが人間の飲む煙草というものである事はようやくこの頃知った。
+　      この書生の掌の裏でしばらくはよい心持に坐っておったが、しばらくすると非常な速力で運転し始めた。書生が動くのか自分
+        だけが動くのか分らないが無暗に眼が廻る。胸が悪くなる。到底助からないと思っていると、どさりと音がして眼から火が出た。それまで
+は記憶しているがあとは何の事やらいくら考え出そうとしても分らない。
+        ''',
+        'https://github.com/osyakan'],
+      ['2', 'assets/programmer.png', 'title2', 'datail2'],
+      ['3', 'assets/programmer.png', 'title3', 'datail3'],
+      ['4', 'assets/programmer.png', 'title4', 'datail4'],
+      ['4', 'assets/programmer.png', 'title5', 'datail5'],
+      ['4', 'assets/programmer.png', 'title6', 'datail6'],
+      ['4', 'assets/programmer.png', 'title7', 'datail7'],
+      ['4', 'assets/programmer.png', 'title8', 'datail8'],
+      ['4', 'assets/programmer.png', 'title9', 'datail9'],
+      ['4', 'assets/programmer.png', 'title10', 'datail10'],
+      ['4', 'assets/programmer.png', 'title11', 'datail11'],
+      ['4', 'assets/programmer.png', 'title12', 'datail12'],
+      ['4', 'assets/programmer.png', 'title13', 'datail13'],
     ];
 
     return Scaffold(
@@ -123,10 +154,10 @@ class _MyHomePageState extends State<MyHomePage> {
                       //   type: PageTransitionType.leftToRight,
                       // ),
                       PageRouteBuilder(
-                        pageBuilder: (context, animation, secondaryAnimation) => SecondRoute(),
+                        pageBuilder: (context, animation, secondaryAnimation) => SecondRoute(i.value),
                         transitionsBuilder: (context, animation, secondaryAnimation, child) {
                           return OpenUpwardsPageTransitionsBuilder().buildTransitions(
-                              MaterialPageRoute(builder: (context)=>SecondRoute()),
+                              MaterialPageRoute(builder: (context)=>SecondRoute(i.value)),
                               context,
                               animation,
                               secondaryAnimation,
