9d06743c9e9691e9088ee020ae23e90935ee32f2
osyakan <kan86yeaoh@gmail.com>
2024-09-23 14:45:42 +0900

    commit

--- website/lib/pages/home_page.dart
+++ website/lib/pages/home_page.dart
@@ -3,8 +3,37 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 200, // 固定幅
+                  height: 200, // 固定高さ
+                  child: Image.asset('image/kan.png'),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Welcome to the Home Page!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
 }
